c0a59961bb0703f70c8f5b56821dc4600ce24840
Jesse <Jesse.mena4@gmail.com>
2019-03-01 15:44:34 -0700

    git starter project renamed

---
+++ BootMVCTiresStore/src/main/java/com/skilldistillery/tires/BootMvcTiresStoreApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.tires;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcTiresStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcTiresStoreApplication.class, args);
+	}
+
+}
