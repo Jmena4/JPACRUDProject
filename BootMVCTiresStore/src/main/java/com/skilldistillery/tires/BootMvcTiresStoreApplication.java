6f653167988e485cc465ae6eb16232326c851d23
Jesse <Jesse.mena4@gmail.com>
2019-03-01 16:24:16 -0700

    Updates: created controllers

--- BootMVCTiresStore/src/main/java/com/skilldistillery/tires/BootMvcTiresStoreApplication.java
+++ BootMVCTiresStore/src/main/java/com/skilldistillery/tires/BootMvcTiresStoreApplication.java
@@ -2,8 +2,10 @@ package com.skilldistillery.tires;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.skilldistillery.tiredata")
 public class BootMvcTiresStoreApplication {
 
 	public static void main(String[] args) {
---
+++ BootMVCTiresStore/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MVC Tire Store</title>
+</head>
+<body>
+<h1>Welcome to Tire Store!</h1>
+<!-- <a href="https://truckfreighter.com/wp-content/uploads/2018/06/best-drive-tires-for-semi-truck.jpg"></a>  -->
+</body>
+</html>
