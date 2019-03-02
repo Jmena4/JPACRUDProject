effac446fed55b3490b8cbe27f2d29d9f498f733
Jesse <Jesse.mena4@gmail.com>
2019-03-02 13:50:33 -0700

    Update: started building the TireController to processing information

--- BootMVCTiresStore/src/main/java/com/skilldistillery/tires/controllers/TireController.java
+++ BootMVCTiresStore/src/main/java/com/skilldistillery/tires/controllers/TireController.java
@@ -1,14 +1,22 @@
 package com.skilldistillery.tires.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.skilldistillery.tires.data.TireDAO;
 
 @Controller
 public class TireController {
 
+	@Autowired
+	private TireDAO tireDAO;
+
 	@RequestMapping(path = "/", method = RequestMethod.GET)
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("allTires", tireDAO.findAll());
 		return "WEB-INF/index.jsp";
 	}
 }
