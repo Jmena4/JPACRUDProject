27184e874606ec00eb16c2d259e5301af7474fac
Jesse <Jesse.mena4@gmail.com>
2019-03-02 17:51:04 -0700

    Refactor: changed the code to allow CRUD @RequestMapping mthods

--- BootMVCTiresStore/src/main/java/com/skilldistillery/tires/controllers/TireController.java
+++ BootMVCTiresStore/src/main/java/com/skilldistillery/tires/controllers/TireController.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.tires.controllers;
 
+import java.sql.SQLException;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,9 +31,45 @@ public class TireController {
 	public ModelAndView getTire(@RequestParam("tid") int tid) {
 		ModelAndView mv = new ModelAndView();
 		Tire tire = tireDAO.findById(tid);
-
 		mv.addObject("tire", tire);
 		mv.setViewName("WEB-INF/tire/show.jsp");
+		return mv;
+	}
+
+	@RequestMapping(path = "addTire.do", method = RequestMethod.POST)
+	public ModelAndView createTire(@Valid Tire tire, int id) throws SQLException {
+		ModelAndView mv = new ModelAndView();
+		boolean isTireAddedSuccessful = tireDAO.create(id);
+
+		if (!isTireAddedSuccessful) {
+			mv.setViewName("WEB-INF/errorPage.jsp");
+			return mv;
+		} else {
+			mv.setViewName("WEB-INF/tire/show.jsp");
+			return mv;
+		}
+
+	}
+
+	@RequestMapping(path = "editTire.do", method = RequestMethod.GET)
+	public ModelAndView editTire(int tireId) throws SQLException {
+		ModelAndView mv = new ModelAndView();
+		Tire updateTireObj = tireDAO.findById(tireId);
+		System.out.println(updateTireObj);
+		mv.addObject("tire", updateTireObj);
+		mv.setViewName("WEB-INF/index.jsp");
+		return mv;
+	}
+
+	@RequestMapping(path = "deleteFilm.do", method = RequestMethod.POST)
+	public ModelAndView deleteTire(int tireId) throws SQLException {
+		ModelAndView mv = new ModelAndView();
+		boolean isSuccessful = tireDAO.delete(tireId);
+		mv.addObject("removed", isSuccessful);
+		mv.setViewName("WEB-INF/index.jsp");
+		if (!isSuccessful) {
+			return null;
+		}
 		return mv;
 	}
 }
